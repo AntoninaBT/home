fea3a48634762e06594aeddb28bdd4879d61fa8e
Антонина <tonyateplova@mail.ru>
2024-11-17 14:47:37 +0300

    17/11/24

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+# Teplova
+def divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    return first / second
+#print(divide(9,0))
---
+++ true_math.py
@@ -0,0 +1,7 @@
+# Teplova
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return inf
+    return first / second
+#print(divide(9,0))
